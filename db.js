293a19c95071a2edcebeff6b2e3c6c0310fb7578
Quinton30 <122327951+Quinton30@users.noreply.github.com>
2025-04-25 17:34:01 +0300

    use new database

--- db.js
+++ db.js
@@ -1,10 +1,20 @@
 const { Pool } = require('pg')
 
-const pool = new Pool({
+/*const pool = new Pool({
   user: 'students_fqe5_user',
   host: 'cvi01adrie7s73ebi45g-a.oregon-postgres.render.com',
   database: 'students_fqe5',
   password: 'Rqur05qinOVXKi1EEbi3wE8XOAHczozY',
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+}) Expired Database*/
+const pool = new Pool({
+  user: 'postgres.auqsxkehosbvhviedypm',
+  host: 'aws-0-us-east-2.pooler.supabase.com',
+  database: 'postgres',
+  password: '#5@w$MM5SdSG8CK',
   port: 5432,
   ssl: {
     rejectUnauthorized: false,
